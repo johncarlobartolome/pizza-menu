89800c80829482a3190387792ea9f9503a4cb620
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-03-10 13:38:31 +0800

    Add store availability function

--- src/index.js
+++ src/index.js
@@ -67,6 +67,13 @@ const Menu = () => {
 };
 
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  console.log(isOpen);
+
   return (
     <footer>{new Date().toLocaleTimeString()}, We're currently open</footer>
   );
