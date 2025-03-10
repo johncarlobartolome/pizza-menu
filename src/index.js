1aa56bb42a977b17268bd9a2b2692eda5c44b3e0
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-03-11 07:15:38 +0800

    Add fragment to wrap shop description and pizza menu

--- src/index.js
+++ src/index.js
@@ -68,11 +68,17 @@ const Menu = () => {
     <main className="menu">
       <h2>Our Menu</h2>
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 5 creative dishes to choose from. All
+            from our store oven, all organic, all delicious.
+          </p>{" "}
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu.</p>
       )}
