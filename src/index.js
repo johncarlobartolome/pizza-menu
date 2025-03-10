c33f7fec02c2473855d08227d7150b1b831cce4b
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-03-10 11:34:52 +0800

    Initial setup for the pizza menu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzas = [
+  {
+    name: "Margherita",
+    ingredients: "Tomato, Mozzarella, Basil",
+    price: 8,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pepperoni",
+    ingredients: "Tomato, Mozzarella, Pepperoni",
+    price: 10,
+    photoName: "pizzas/pepperoni.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Hawaiian",
+    ingredients: "Tomato, Mozzarella, Ham, Pineapple",
+    price: 11,
+    photoName: "pizzas/hawaiian.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Veggie Delight",
+    ingredients: "Tomato, Mozzarella, Bell Peppers, Olives, Onions, Mushrooms",
+    price: 9,
+    photoName: "pizzas/veggie-delight.jpg",
+    soldOut: true,
+  },
+  {
+    name: "BBQ Chicken",
+    ingredients: "BBQ Sauce, Mozzarella, Chicken, Red Onions, Cilantro",
+    price: 12,
+    photoName: "pizzas/bbq-chicken.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/margherita.jpg" alt="Margherita Pizza" />
+      <h2>Margherita Pizza</h2>
+      <p>Tomato, Mozzarella, Basil</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
